9a11b84eae426d0293272bf46193991409e65e3f
Radosław Osytek <Osytek96@gmail.com>
2021-05-26 03:29:08 +0200

    Add columns to section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
-import Hero from '../Hero/Hero.js';
 
 class App extends React.Component {
   render() {
@@ -9,8 +8,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
-        <Hero />
+        <List title={['Things to do ', <sup>soon!</sup>]} imageSrc="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png">
+        
+        </List>
       </main>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+    }
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+      }
   render() {
     return (
       <section className={styles.component}>
-        <h2>Dowolna treść</h2>
+        <Hero titleText={this.props.title} imageSrc={this.props.imageSrc}/>
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
+        <div className={styles.columns}>
+            <Column columnTitle ='Animals'></Column>
+            <Column columnTitle ='Minerals'></Column>
+            <Column columnTitle ='Films'></Column>
+        </div>
       </section>
     )
   }
